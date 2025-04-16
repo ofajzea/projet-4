b132f22c150be11597fd5e4970c3c37b288ef85d
test <test@test.fr>
2025-04-16 11:43:29 +0200

    Updated install.sh for automatic token

--- projects/subjects/05_forest/install.sh
+++ projects/subjects/05_forest/install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ];
+  then
+  export HUGGINGFACE_TOKEN=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
+elif [ $# -gt 0 ];
+  then
+  export HUGGINGFACE_TOKEN=$1
+else
+  echo "Usage: source $0 <token>  OR source $0"
+  exit 1
+fi
+
 conda create -n project_teledetection_05 python=3.11
 conda activate project_teledetection_05
 pip install -e .
-export HUGGINGFACE_TOKEN=$1
 python src/data_loading.py
--- projects/subjects/06_irradiance/install.sh
+++ projects/subjects/06_irradiance/install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [ $# -eq 0 ];
+  then
+  token=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
+elif [ $# -gt 0 ];
+  then
+  token=$1
+else
+  echo "Usage: source $0 <token>  OR source $0"
+  exit 1
+fi
+
 conda create -n project_teledetection_06 python=3.11
 conda activate project_teledetection_06
 pip install -e .
-export HUGGINGFACE_TOKEN=$1
+export HUGGINGFACE_TOKEN=token
 python src/data_loading.py
